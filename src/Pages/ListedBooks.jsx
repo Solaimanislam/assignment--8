8c5d7f618d650ea9f648864b168188a571cc971a
Solaimanislam <islamsi2001@gmail.com>
2024-03-27 03:06:13 +0600

    added read and wish list

--- src/Pages/ListedBooks.jsx
+++ src/Pages/ListedBooks.jsx
@@ -1,41 +1,48 @@
 import { useState } from "react";
 import { Link, Outlet, useNavigation } from "react-router-dom";
 import Loader from "../Components/Loader";
-
+import { IoIosArrowDown } from "react-icons/io";
 
 
 const ListedBooks = () => {
     const [tabIndex, setTabIndex] = useState(0);
     const navigation = useNavigation();
-    if(navigation.state === 'loading') return <Loader></Loader>
-    return ( 
+    if (navigation.state === 'loading') return <Loader></Loader>
+    return (
         <div>
-            <details className="dropdown ">
-                <summary className="m-1 btn">open or close</summary>
-                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
-                </ul>
-            </details>
+            <div className=" flex justify-center items-center bg-slate-300 lg:py-16 rounded-xl">
+                <h1 className=" lg:text-4xl lg:font-bold ">Books</h1>
+            </div>
+            <div className="flex justify-center items-center">
+                <details className="dropdown ">
+                    <summary className="m-1 btn bg-[#23BE0A] text-white px-10">Sort By<IoIosArrowDown></IoIosArrowDown> </summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <li><a>Sort By</a></li>
+                        <li><a>Rating</a></li>
+                        <li><a>Number of Pages</a></li>
+                        <li><a>Published Year</a></li>
+                    </ul>
+                </details>
+            </div>
             <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start lg:mx-32 flex-nowrap dark:bg-gray-100 dark:text-gray-800">
                 <Link
-                 to='' 
-                onClick={()=>setTabIndex(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
+                    to=''
+                    onClick={() => setTabIndex(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                         <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                     </svg>
                     <span>Read Books</span>
                 </Link>
-                <Link 
-                to={`wishlistBooks`}
-                onClick={()=>setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
+                <Link
+                    to={`wishlistBooks`}
+                    onClick={() => setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                         <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                         <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                     </svg>
                     <span>Wishlist Books</span>
                 </Link>
-               
+
             </div>
             <Outlet></Outlet>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes'
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 
 
